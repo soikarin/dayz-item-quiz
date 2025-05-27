6515394dbfe767291ea78e3b42b245bfff782b5c
Santeri Oikarinen <santeri.oikarinen@student.oulu.fi>
2025-05-27 16:34:51 +0300

    Changed the list of items to a separate file

---
+++ items.js
@@ -0,0 +1,44 @@
+const items = [
+	{
+		category: "weapons",
+		image: "./images/weapons/AK74.webp",
+		acceptedAnswers: ["ak-74", "ak74", "ka-74", "ka74", "ak 74", "ka 74", "5.45 ak", "5.45 ka", "5.45x39mm ak", "5.45x39mm ka"],
+		itemName: "KA-74"
+	},
+	{
+		category: "weapons",
+		image: "./images/weapons/AK101.webp",
+		acceptedAnswers: ["ak-101", "ak101", "ka-101", "ka101", "ak 101", "ka 101", "5.56 ka", "5.56x45mm ka", "5.56 ak", "5.56x45mm ak"],
+		itemName: "KA-101"
+	},
+	{
+		category: "weapons",
+		image: "./images/weapons/AKM.webp",
+		acceptedAnswers: ["ak-47", "ka-m", "ak-m", "akm", "kam", "ak 47", "ka 47", "kalashnikov", "kalash"],
+		itemName: "KA-M"
+	},
+	{
+		category: "weapons",
+		image: "./images/weapons/AKS74U.webp",
+		acceptedAnswers: ["paska ak", "paskaak", "kassi", "aks74u", "kas-74u", "aks-74u", "mini ak"],
+		itemName: "KAS-74U"
+	},
+	{
+		category: "weapons",
+		image: "./images/weapons/ASVAL.webp",
+		acceptedAnswers: ["asval", "sval", "as val", "s val"],
+		itemName: "SVAL"
+	},
+	{
+		category: "weapons",
+		image: "./images/weapons/Aug.webp",
+		acceptedAnswers: ["aur ax", "aurax", "pitkä aug", "aug a3", "auga3", "aug ax", "steyr aug a3"],
+		itemName: "AUR AX"
+	},
+	{
+		category: "weapons",
+		image: "./images/weapons/AugShort.webp",
+		acceptedAnswers: ["aur a1", "aug a1", "lyhyt aug", "steyr aug a1", "paska aug"],
+		itemName: "AUR A1"
+	}
+];
